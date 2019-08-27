03b501f6d2c4d4b3a830d744bfa66238ed1f92d8
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-27 13:06:58 +0430

    put foundation of context api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import GithubState from './context/github/GithubState';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <GithubState>
+    <Router>
+      <App />
+    </Router>
+  </GithubState>,
   document.getElementById('root')
 );
